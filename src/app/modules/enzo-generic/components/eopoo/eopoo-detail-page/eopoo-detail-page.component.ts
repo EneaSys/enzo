2dd49672b7202f265f7a27289276a918311d6273
Cavallaro Catello Stefano <stefano@stepo.biz>
2022-02-01 18:14:28 +0100

    fix bug of tab name

--- src/app/modules/enzo-generic/components/eopoo/eopoo-detail-page/eopoo-detail-page.component.ts
+++ src/app/modules/enzo-generic/components/eopoo/eopoo-detail-page/eopoo-detail-page.component.ts
@@ -1,49 +1,49 @@
 import { Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { EopooDTO, EopooResourceService} from 'aig-generic';
+import { EopooDTO, EopooResourceService } from 'aig-generic';
 import { AgalCommonService } from "@agal-core/services/common.service";
 import { EnzoGenericComponent1 } from "app/shared/generic1.component";
 import { EnzoGenericComponentService1 } from "app/shared/generic-component1.service";
 import { EopooNamePipe } from "@agal-generic/pipe/eopoo-name.pipe";
 
 @Component({
-  selector: 'enzo-eopoo-detail-page',
-  templateUrl: './eopoo-detail-page.component.html',
-  styleUrls: ['./eopoo-detail-page.component.scss']
+	selector: 'enzo-eopoo-detail-page',
+	templateUrl: './eopoo-detail-page.component.html',
+	styleUrls: ['./eopoo-detail-page.component.scss']
 })
 export class EnzoEopooDetailPageComponent extends EnzoGenericComponent1 {
-  constructor(
-    private resourceService: EopooResourceService,
-    private eopooName: EopooNamePipe,
-    private route: ActivatedRoute,
-    public _gcs: EnzoGenericComponentService1,
-    agcs: AgalCommonService,
-  ) {
-      super(_gcs);
-      this.onLoad();
-    }
+	constructor(
+		private resourceService: EopooResourceService,
+		private eopooName: EopooNamePipe,
+		private route: ActivatedRoute,
+		public _gcs: EnzoGenericComponentService1,
+		agcs: AgalCommonService,
+	) {
+		super(_gcs);
+		this.id = +this.route.snapshot.paramMap.get('id');
+		this.tmCurrentComponent.name = this.eopooName.transform(this.route.snapshot.data.eopoo);
+		this.onLoad();
+	}
 
-  id: number;
+	id: number;
 
-  eopooDTO: EopooDTO;
+	eopooDTO: EopooDTO;
 
-  onLoad() {
-    this.id = +this.route.snapshot.paramMap.get('id');
-    this.tmCurrentComponent.name = this.eopooName.transform(this.route.snapshot.data.eopoo);
-    this.eopooDTO = this.route.snapshot.data.eopoo;
-  } 
+	onLoad() {
+		this.eopooDTO = this.route.snapshot.data.eopoo;
+	}
 
-  async reloadPage() {
-    this.eopooDTO = await this.resourceService.getEopooUsingGET(this.eopooDTO.id).toPromise();
-  }
+	async reloadPage() {
+		this.eopooDTO = await this.resourceService.getEopooUsingGET(this.eopooDTO.id).toPromise();
+	}
 
-  /*editEopoo(eopoo: EopooDTO) {
-    //this.dialog.open(EnzoEopooNewUpdateDialogComponent, { data: { eopoo: eopoo } });
-  }
-
-  async deleteEopoo(eopooDTO: EopooDTO) {
-    
-  }*/
+	/*editEopoo(eopoo: EopooDTO) {
+	  //this.dialog.open(EnzoEopooNewUpdateDialogComponent, { data: { eopoo: eopoo } });
+	}
+  
+	async deleteEopoo(eopooDTO: EopooDTO) {
+	  
+	}*/
 }
 
 
