cc8f99a8c70b3175cb8cdb0a81525108d26cc010
Cavallaro Catello Stefano <stefano@stepo.biz>
2022-02-11 16:37:24 +0100

    fix reference

--- src/app/modules/example/example.module.ts
+++ src/app/modules/example/example.module.ts
@@ -5,9 +5,9 @@ import { EnzoSharedModule } from 'app/shared/shared.module';
 
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ExampleComponent } from './components/example/example.component';
-import { AgalItalianLegislationModule } from '@agal-italianlegislation/agal-italianlegislation.module';
 import { EnzoDetailPageComponent } from './components/detail-page/detail-page.component';
 import { EnzoListPageComponent } from './components/list-page/list-page.component';
+import { AgalItalianLegislationModule } from '@agal-italian-legislation/italian-legislation.module';
 
 const exampleRoutes: Route[] = [
     {
